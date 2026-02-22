c8a29f86767d859939cd6439b48f6d060f513491
Gabor Jonas <gaborjonas8813@gmail.com>
2026-02-22 14:24:35 +0000

    Fix missing method from InterestAccountServiceInterface

--- src/InterestAccountServiceInterface.php
+++ src/InterestAccountServiceInterface.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace Chip\InterestAccount;
 
 use Chip\InterestAccount\Domain\Aggregate\Account;
+use Chip\InterestAccount\Domain\Exception\DomainException;
+use Chip\InterestAccount\Domain\Projection\Transaction;
 
 interface InterestAccountServiceInterface
 {
@@ -14,4 +16,10 @@ interface InterestAccountServiceInterface
      * @param numeric-string $amount
      */
     public function deposit(string $accountId, string $userId, string $amount): Account;
+
+    /**
+     * @return list<Transaction>
+     * @throws DomainException
+     */
+    public function listAccountStatement(string $accountId): array;
 }
